7d9ca1aae1450e70eab6675765123a37d5640c08
Jason Reeder <info@jasonreeder.com>
2019-02-24 22:53:00 -0800

    Fetch ingredients

--- alphabet-soup/src/app/soup/soup.page.ts
+++ alphabet-soup/src/app/soup/soup.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-soup',
@@ -10,13 +12,22 @@ import { map } from 'rxjs/operators';
 })
 export class SoupPage implements OnInit {
   id: Observable<string>;
+  ingredients: Observable<any>;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
     this.id = this.route.params.pipe(
       map(params => params.id)
     );
-  }
 
+    const userId = this.afAuth.auth.currentUser.uid;
+    this.ingredients = this.afs
+      .collection(`users/${userId}/soups/${this.id}`)
+      .valueChanges();
+  }
 }
