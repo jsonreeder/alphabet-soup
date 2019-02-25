85d465094e4558ab39a0db070fb020dfb98da8a5
Jason Reeder <info@jasonreeder.com>
2019-02-24 22:06:42 -0800

    Add actual API call

--- functions/src/handler.ts
+++ functions/src/handler.ts
@@ -1,7 +1,15 @@
 import * as functions from 'firebase-functions';
 import { firestore } from 'firebase-admin';
+import fetch = require('node-fetch');
 
-export function handler(snapshot: firestore.DocumentSnapshot, context: functions.EventContext): boolean {
-  console.log(context.params);
-  return true;
+export async function handler(
+  snapshot: firestore.DocumentSnapshot,
+  context: functions.EventContext
+  ): Promise<boolean> {
+    console.log(context.params);
+    const response = await fetch('http://ron-swanson-quotes.herokuapp.com/v2/quotes');
+    const json = await response.json();
+    console.log(json);
+    console.log(json[0]);
+    return true;
 }
