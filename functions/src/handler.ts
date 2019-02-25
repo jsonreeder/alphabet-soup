5d480f2cfeb6632a7b22bef79d741c2afe72ce5b
Jason Reeder <info@jasonreeder.com>
2019-02-24 21:57:22 -0800

    Add return

--- functions/src/handler.ts
+++ functions/src/handler.ts
@@ -1,6 +1,7 @@
 import * as functions from 'firebase-functions';
 import { firestore } from 'firebase-admin';
 
-export function handler(snapshot: firestore.DocumentSnapshot, context: functions.EventContext) {
+export function handler(snapshot: firestore.DocumentSnapshot, context: functions.EventContext): boolean {
   console.log(context.params);
+  return true;
 }
